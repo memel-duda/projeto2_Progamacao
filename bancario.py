20e284fd3a7c5251a7db4e3e716a285333621e9d
Aline345-RS <AlinesilvaRodrigues4500@gmail.com>
2025-10-30 09:30:24 -0300

    o máximo de atualização na estrutura do projeto
    estaremos desenvolvendo de tarde

--- bancario.py
+++ bancario.py
@@ -6,6 +6,71 @@ class Cliente:
         self.cpf=cpf
         self.data_nascimento=data_nascimento
         self.endereco=endereco
+        
+    try:
+       with open(__aquivo,"r",encoding="utf-8") as arquivo:
+            clientes=json.load(arquivo)
+
+
+    except FileNotFoundError:
+        with open(__aquivo,"w",encoding="utf-8") as arquivo:
+            json.dump([],arquivo)
+
+    
+    def salvar_cliente(self):
+
+        """Salva ou atualizar o Cliente ao json"""
+        novo_cliente={
+
+            "nome":self.nome,
+            "cpf":self.cpf,
+            "data_nascimento":self.data_nascimento,
+            "endereco":self.endereco
+        }
+
+        with open(self.__aquivo,"r",encoding="utf-8") as arquivo:
+            clientes=json.load(arquivo)
+            clientes.append(novo_cliente)
+            
+            atualizado=False
+            for c in clientes: #percorre a cda clinte já salvo
+                if c["cpf"]==self.cpf:# se encontra clinte com o mesmo cpf
+                    c["nome"]=self.nome # atualiza o nome
+                    c["data_nascimento"]=self.data_nascimento #atualiza a data de nascimento
+                    c["endereco"]=self.endereco #atualiza o endereco
+                    atualizado=True #marca como atualizado
+
+            if not atualizado:#se não obteve cliente com o mesmo cpf
+                clientes.append(novo_cliente) #adiciona novo cliente a lista
+
+        with open(self.__aquivo,"w",encoding="utf-8") as arquivo:
+            json.dump(clientes,arquivo,indent=4)
+            
+            if atualizado:
+                return "Cliente atualizado com sucesso!"
+            else:
+                print(f"Cliente {self.nome} cadastrado com sucesso!")
+
+        @classmethod
+        def listar_clientes(cls):
+
+            """Lista todos os clientes salvos no json"""
+
+            with open(cls.__aquivo,"r",encoding="utf-8") as arquivo:
+                clientes=json.load(arquivo)
+                return clientes
+            
+        @classmethod
+        def excluir_cliente(cls,cpf):
+             
+            with open(cls.__aquivo,"r",encoding="utf-8") as arquivo:
+                clientes=json.load(arquivo)
+                clientes_atualizados=[c for c in clientes if c["cpf"]!=cpf]
+
+            with open(cls.__aquivo,"w",encoding="utf-8") as arquivo:
+                json.dump(clientes_atualizados,arquivo,indent=4)
+                return f"Cliente com CPF {cpf} excluido com sucesso!"
+
 
 class Conta:
     def __init__(self,cliente,saldo,senha):
